2810e3541f33d4fbb8fb85cb4714201f8628bbc4
Thierry Heinz <studioklatz@gmail.com>
2026-07-09 10:58:16 +0200

    added better-auth config

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from '../src/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { nextCookies } from 'better-auth/next-js';
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({ adapter });
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
